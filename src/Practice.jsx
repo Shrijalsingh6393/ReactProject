420cf319e267d255f5c28cd063f68655cf423522
Shrijal Singh <150268923+Shrijalsingh6393@users.noreply.github.com>
2025-12-15 15:50:21 +0530

    Update Practice.jsx

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,22 +1,22 @@
-// Create a button with toggle ON/OFF and also update the status in the heading . Style the div section 
-// with CSS Module
+// // Create a button with toggle ON/OFF and also update the status in the heading . Style the div section 
+// // with CSS Module
 
 
-import React,{useState} from 'react'
-// import style from './PracticeCSSModule.module.css'
+// import React,{useState} from 'react'
+// // import style from './PracticeCSSModule.module.css'
 
-const Practice = () => {
-    const [toggle,setToggle] = useState("ON")
+// const Practice = () => {
+//     const [toggle,setToggle] = useState("ON")
 
-    const toggleChange = () => {
-        setToggle(pre=>pre=="ON"?"OFF":"ON")
-    }
-  return (
-    <div className={style.cont}>
-      <h2>Value: {toggle}</h2>
-      <button className={style.sty} onClick={toggleChange}>Toggle {toggle=='ON'?'OFF':'ON'}</button>
-    </div>
-  )
-}
+//     const toggleChange = () => {
+//         setToggle(pre=>pre=="ON"?"OFF":"ON")
+//     }
+//   return (
+//     <div className={style.cont}>
+//       <h2>Value: {toggle}</h2>
+//       <button className={style.sty} onClick={toggleChange}>Toggle {toggle=='ON'?'OFF':'ON'}</button>
+//     </div>
+//   )
+// }
 
-export default Practice
+// export default Practice
